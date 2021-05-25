cbaa8a1856823d8e3b9167ff7e614ae6ac20ee9e
qiyao.kxp <qiyao.kxp@alibaba-inc.com>
2021-05-25 10:13:54 +0800

    feat: add test page

---
+++ pages/test.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Test() {
+  return (
+    <div>
+      <Head>
+        <title>Test</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>A Test Page</div>
+    </div>
+  )
+}
